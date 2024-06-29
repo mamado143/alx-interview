c51510ba43f920eeba11a64382567dcb0cfcb028
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-06-29 09:22:56 +0300

    Solve problem pascal

--- alx-interview/0-pascal_triangle.py
+++ alx-interview/0-pascal_triangle.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 '''A module for working with Pascal's triangle.
 '''
-
-
 def pascal_triangle(n):
     '''Creates a list of lists of integers representing
     the Pascal's triangle of a given integer.
@@ -17,5 +15,5 @@ def pascal_triangle(n):
                 line.append(1)
             elif i > 0 and j > 0:
                 line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        triangle.append(line)
-    return triangle
+                triangle.append(line)
+                return triangle
